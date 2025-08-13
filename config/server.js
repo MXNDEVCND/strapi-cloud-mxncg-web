14bd4d5b707fb142dbd8024cab28b01a1f11aaa7
Joseph Kim <howtextworks@gmail.com>
2025-08-13 19:36:48 -0400

    환경 설정 파일 및 타입 정의 수정: jsconfig.json에 NodeNext 모듈 추가, admin.js 및 server.js에서 기본값 설정, 타입 정의에서 컴포넌트 및 콘텐츠 타입 이름 변경

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']),
+  },
+  encryption: {
+    key: env('ENCRYPTION_KEY', 'defaultEncryptionKey'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
